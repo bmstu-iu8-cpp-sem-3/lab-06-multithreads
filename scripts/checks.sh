0f3289c8524955e890b795b653d3095d88567d83
progrm_jarvis <gh@progrm-jarvis.ru>
2020-11-08 20:02:01 +0300

    build(CI): specify exact paths to be checked by checks.sh

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-files=$(find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*")
+files=$(find ./include/ ./scripts/ ./sources/ -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*")
 filter=-build/c++11,-runtime/references,-runtime/indentation_namespace,-whitespace/braces,-whitespace/indent,\
 -whitespace/comments,-whitespace/line_length,-build/include_order
 echo $files | xargs cpplint --filter=$filter
